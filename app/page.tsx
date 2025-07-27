4f1b7b4814cd86546945b23a0ff3710de02f3b31
Gian Handa <gianhanda2010@gmail.com>
2025-07-26 22:36:06 -0400

    Visual fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,27 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className="left-content">
+      <div className={styles.leftContent}>
         <h1>Gian Handa</h1>
-        <div className="social-buttons">
-          <a href="https://github.com/yourusername" target="_blank">GitHub</a>
-          <a href="https://linkedin.com/in/yourusername" target="_blank">LinkedIn</a>
-          <a href="https://twitter.com/yourusername" target="_blank">Twitter</a>
+        <div className={styles.socialButtons}>
+          <button onClick={() => window.open('https://github.com/yourusername', '_blank')}>
+            GitHub
+          </button>
+          <button onClick={() => window.open('https://linkedin.com/in/yourusername', '_blank')}>
+            LinkedIn
+          </button>
+          <button onClick={() => window.open('https://twitter.com/yourusername', '_blank')}>
+            Twitter
+          </button>
         </div>
       </div>
-      <div className="photo-container">
+      <div className={styles.photoContainer}>
         <Image
-          src="/gianhanda.jpg" // put your actual photo in the public/ folder
+          src="/gianhanda.jpg" // photo in public folder
           alt="Gian Handa"
           width={300}
           height={300}
-          className="profile-photo"
+          className={styles.profilePhoto}
         />
       </div>
     </main>
